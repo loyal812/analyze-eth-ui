67b0745c20f02183827abf4e7477952c8631209a
loyal812 <jh.chan0812@gmail.com>
2024-07-30 10:24:29 -0400

    feat: show the analyze result feature

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
 const DEV_MODE = true;
-export const API_BASE = DEV_MODE == true ? 'http://localhost:5000/api/v2' : '';
-// export const API_BASE = DEV_MODE == true ? 'https://analyze-eth.onrender.com/api/v2' : '';
+// export const API_BASE = DEV_MODE == true ? 'http://localhost:5000/api/v2' : '';
+export const API_BASE = DEV_MODE == true ? 'https://analyze-eth.onrender.com/api/v2' : '';
--- src/pages/analyze/index.jsx
+++ src/pages/analyze/index.jsx
@@ -5,6 +5,7 @@ import { useDrawingArea } from '@mui/x-charts/hooks';
 import { PieChart } from '@mui/x-charts/PieChart';
 import { useDispatch } from 'react-redux';
 import { getAnalyze, getWalletAnalyze } from '../../redux/analyzeReducer';
+import { setLoading } from '../../redux/globalReducer';
 
 export default function Analyze() {
   const dispatch = useDispatch();
@@ -14,11 +15,7 @@ export default function Analyze() {
   const [holder1, setHolder1] = useState([])
   const [holder2, setHolder2] = useState([])
   const [cross_list, setCrossList] = useState([])
-
-  const overlap_holder_data = [
-    { value: 722, label: 'Only WOLF Holders', color: '#9001CB' }, //, color: '#610094'
-    { value: 268, label: 'Overlap Holders', color: '#DA00FF' }
-  ];
+  const [wallet_info, setWalletInfo] = useState(null)
 
   const size = {
     width: 300,
@@ -34,7 +31,7 @@ export default function Analyze() {
 
   const CustomTextField = styled(TextField)(({ theme }) => ({
     '& .MuiInputBase-input': {
-      color: 'white', 
+      color: 'white',
     },
   }));
 
@@ -48,24 +45,29 @@ export default function Analyze() {
   }
 
   const analyze = async () => {
-    console.log(token_address1)
-    console.log(token_address2)
-
+    dispatch(setLoading(true));
     let result = await dispatch(getAnalyze(token_address1, token_address2))
-    
+
     setHolder1(result[0])
     setHolder2(result[1])
     setCrossList(result[2])
     console.log("analyze result: ", result)
+    dispatch(setLoading(false));
   }
+
+
+  const overlap_holder_data = [
+    { value: 1000 - cross_list.length, label: 'Only WOLF Holders', color: '#9001CB' }, //, color: '#610094'
+    { value: cross_list.length, label: 'Overlap Holders', color: '#DA00FF' }
+  ];
 
   const analyzeWalletAddress = async (wallet_address) => {
-    console.log(wallet_address)
-
+    dispatch(setLoading(true));
     let result = await dispatch(getWalletAnalyze(wallet_address))
-
-    console.log(result)
+    setWalletInfo(result)
+    dispatch(setLoading(false));
   }
+
 
   return (
     <div className='bg-slate-950 w-full p-8 min-h-[100vh] text-white space-y-8'>
@@ -73,31 +75,31 @@ export default function Analyze() {
         <div className='col-span-5'>
           <div className='flex'>
             <p>Token address 1</p>
-            <CustomTextField 
+            <CustomTextField
               value={token_address1}
               onChange={(e) => setTokenAddress1(e.target.value)}
-              variant="standard" 
-              focused 
+              variant="standard"
+              focused
               className="w-full"
               defaultValue="0x66bFf695f3B16a824869a8018a3A6e3685241269" />
           </div>
         </div>
         <div className='col-span-2 text-center'>
-          <Button 
+          <Button
             onClick={() => analyze()}
-            color='secondary' 
+            color='secondary'
             variant="outlined">
-              Analyze
+            Analyze
           </Button>
         </div>
         <div className='col-span-5'>
           <div className='flex'>
             <p>Token address 2</p>
-            <CustomTextField 
+            <CustomTextField
               value={token_address2}
               onChange={(e) => setTokenAddress2(e.target.value)}
-              variant="standard" 
-              focused 
+              variant="standard"
+              focused
               className='w-full'
               defaultValue="0x67466BE17df832165F8C80a5A120CCc652bD7E69" />
           </div>
@@ -111,18 +113,12 @@ export default function Analyze() {
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
               <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-                  <th scope="col" className="px-6 py-3 rounded-s-lg">
+                  <th scope="col" className="px-6 py-3">
                     Wallet Address
                   </th>
                   <th scope="col" className="px-6 py-3">
                     Balance
                   </th>
-                  {/* <th scope="col" className="px-6 py-3">
-                    USD value
-                  </th>
-                  <th scope="col" className="px-6 py-3 rounded-e-lg">
-                    Percentage
-                  </th> */}
                 </tr>
               </thead>
               <tbody className=''>
@@ -159,7 +155,7 @@ export default function Analyze() {
                 <PieChart series={[{ data: overlap_holder_data, innerRadius: 55 }]} {...size} slotProps={{
                   legend: { hidden: true },
                 }}>
-                  <PieCenterLabel>{cross_list.length / holder1.length * 100} %</PieCenterLabel>
+                  <PieCenterLabel>{cross_list.length / 10} %</PieCenterLabel>
                 </PieChart>
               </div>
             </div>
@@ -171,7 +167,7 @@ export default function Analyze() {
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
               <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-                  <th scope="col" className="px-6 py-3 rounded-s-lg">
+                  <th scope="col" className="px-6 py-3">
                     Wallet Address
                   </th>
                   <th scope="col" className="px-6 py-3">
@@ -211,41 +207,262 @@ export default function Analyze() {
       </div>
 
       <div className='grid grid-cols-12'>
-          <div className='col-span-5 space-y-8'>
-            <p className='font-bold text-2xl text-center'>Crossovered holder list</p>
-            <div className='overflow-x-auto max-h-96'>
-              <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
-                  <tr>
-                    <th scope="col" className="px-6 py-3 text-center rounded-lg">
-                      Wallet Address
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    cross_list && cross_list.map(data => (
-                      <tr className="bg-white dark:bg-gray-800">
-                        <th 
-                          scope="row" 
-                          className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap dark:text-white hover:bg-gray-300"
-                          onClick={() => analyzeWalletAddress(data)}
-                        >
-                          {data}
-                        </th>
-                      </tr>
-                    ))
-                  }
-                </tbody>
-              </table>
-            </div>
+        <div className='col-span-5 space-y-8'>
+          <p className='font-bold text-2xl text-center'>Crossovered holder list</p>
+          <div className='overflow-x-auto max-h-[730px]'>
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                  <th scope="col" className="px-6 py-3 text-center">
+                    Wallet Address
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  cross_list && cross_list.map(data => (
+                    <tr className="bg-white dark:bg-gray-800">
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap dark:text-white hover:bg-gray-300"
+                        onClick={() => analyzeWalletAddress(data)}
+                      >
+                        {data}
+                      </th>
+                    </tr>
+                  ))
+                }
+              </tbody>
+            </table>
           </div>
-          <div className='col-span-2'>
+        </div>
+        <div className='col-span-2'>
 
-          </div>
-          <div className='col-span-5'>
-              
-          </div>
+        </div>
+        {
+          wallet_info &&
+          <>
+            <div className='col-span-5 space-y-8'>
+              <p className='font-bold text-2xl text-center'>Information of selected wallet</p>
+              <div className='overflow-x-auto max-h-96'>
+                <div className='flex'>
+                  <p>Wallet Address : </p>
+                  <p>{wallet_info['wallet_address']}</p>
+                </div>
+                <div className='flex'>
+                  <p>Spent amount to buy coin : </p>
+                  <p>{wallet_info['spent_amount']}</p>
+                </div>
+                <div className='flex'>
+                  <p>Holded period of coin : </p>
+                  <p>{wallet_info['time_started']}</p>
+                </div>
+                <div className='flex'>
+                  <p>Balance of Token 1 : </p>
+                  <p>{wallet_info['balance_token1']}</p>
+                </div>
+                <div className='flex'>
+                  <p>Balance of Token 2 : </p>
+                  <p>{wallet_info['balance_token2']}</p>
+                </div>
+              </div>
+              <div>
+                <div>Position Status by time about Token 1</div>
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                  <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                      <th scope="col" className="px-6 py-3 text-center">
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-center">
+                        Token Balance
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-center">
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className="bg-white dark:bg-gray-800 dark:text-white hover:bg-gray-300">
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        Value
+                      </th>
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        {wallet_info && wallet_info.balance_token1}
+                      </th>
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                      </th>
+                    </tr>
+                    <tr className="bg-white dark:bg-gray-800 dark:text-white hover:bg-gray-300">
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        24h
+                      </th>
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        {wallet_info && wallet_info.token_balances_day_ago[0].token_balance}
+                      </th>
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        <span className='text-red-500'> [ {(wallet_info.token_balances_day_ago[0]['token_balance'] - wallet_info['balance_token1']) / wallet_info['balance_token1'] * 100} % ] </span>
+                      </th>
+                    </tr>
+                    <tr className="bg-white dark:bg-gray-800 dark:text-white hover:bg-gray-300">
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        7d
+                      </th>
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        {wallet_info && wallet_info.token_balances_week_ago[0].token_balance}
+                      </th>
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        <span className='text-red-500'> [ {(wallet_info.token_balances_week_ago[0]['token_balance'] - wallet_info['balance_token1']) / wallet_info['balance_token1'] * 100} % ] </span>
+                      </th>
+                    </tr>
+                    <tr className="bg-white dark:bg-gray-800 dark:text-white hover:bg-gray-300">
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        30d
+                      </th>
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        {wallet_info && wallet_info.token_balances_month_ago[0].token_balance}
+                      </th>
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        <span className='text-red-500'> [ {(wallet_info.token_balances_month_ago[0]['token_balance'] - wallet_info['balance_token1']) / wallet_info['balance_token1'] * 100} % ] </span>
+                      </th>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div>
+                <div>Position Status by time about Token 2</div>
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                  <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                      <th scope="col" className="px-6 py-3 text-center">
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-center">
+                        Token Balance
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-center">
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className="bg-white dark:bg-gray-800 dark:text-white hover:bg-gray-300">
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        Value
+                      </th>
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        {wallet_info && wallet_info.balance_token2}
+                      </th>
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                      </th>
+                    </tr>
+                    <tr className="bg-white dark:bg-gray-800 dark:text-white hover:bg-gray-300">
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        24h
+                      </th>
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        {wallet_info && wallet_info.token_balances_day_ago[1].token_balance}
+                      </th>
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        <span className='text-red-500'> [ {(wallet_info.token_balances_day_ago[1]['token_balance'] - wallet_info['balance_token2']) / wallet_info['balance_token2'] * 100} % ] </span>
+                      </th>
+                    </tr>
+                    <tr className="bg-white dark:bg-gray-800 dark:text-white hover:bg-gray-300">
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        7d
+                      </th>
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        {wallet_info && wallet_info.token_balances_week_ago[1].token_balance}
+                      </th>
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        <span className='text-red-500'> [ {(wallet_info.token_balances_week_ago[1]['token_balance'] - wallet_info['balance_token2']) / wallet_info['balance_token2'] * 100} % ] </span>
+                      </th>
+                    </tr>
+                    <tr className="bg-white dark:bg-gray-800 dark:text-white hover:bg-gray-300">
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        30d
+                      </th>
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        {wallet_info && wallet_info.token_balances_month_ago[1].token_balance}
+                      </th>
+                      <th
+                        scope="row"
+                        className="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        <span className='text-red-500'> [ {((wallet_info.token_balances_month_ago[1]['token_balance'] - wallet_info['balance_token2']) / wallet_info['balance_token2'] * 100) * 100 / 100} % ] </span>
+                      </th>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </>
+        }
       </div>
     </div>
   );
